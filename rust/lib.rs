83a66e2f8be0c6f50fb74ededbf4cb162c313620
Erik Tollerud <erik.tollerud@gmail.com>
2023-09-27 01:01:43 +0100

    added buffer-based rust implementation

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,20 +1,10 @@
 use numpy::ndarray::{ArrayD, ArrayViewD, ArrayViewMutD, Axis, Array};
 use numpy::{IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn};
+use pyo3::buffer::PyBuffer;
 use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
 #[pymodule]
-fn rust_stats<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
-    // example using immutable borrows producing a new array
-    fn axpy(a: f64, x: ArrayViewD<'_, f64>, y: ArrayViewD<'_, f64>) -> ArrayD<f64> {
-        a * &x + &y
-    }
-
-    // example using a mutable borrow to modify an array in-place
-    fn mult(a: f64, mut x: ArrayViewMutD<'_, f64>){
-        x *= a;
-        println!("first is {}", x[0]);
-        x[0] = x[0] + 4f64;
-    }
+fn rust_stats<'py>(py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
 
     fn ks_2samp(data1: ArrayViewD<'_, f64>, 
                 data2: ArrayViewD<'_, f64>) -> f64 {
@@ -24,10 +14,6 @@ fn rust_stats<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
         let inv_n1 = 1f64 / (n1 as f64);
         let inv_n2 = 1f64 / (n2 as f64);
 
-        let dvec1:Vec<f64> = data1.iter().cloned().collect();
-        let dvec2:Vec<f64> = data2.iter().cloned().collect();
-        
-        
         let mut d = 0f64;
         let mut mind = 0f64;
         let mut maxd = 0f64;
@@ -40,14 +26,14 @@ fn rust_stats<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
             let d2j = data2[j];
 
             if d1i <= d2j {
-                while (i < n1) && (dvec1[i] == d1i) {
+                while (i < n1) && (data1[i] == d1i) {
                     d += inv_n1;
                     i += 1;
                 }
             }
 
             if d1i >= d2j {
-                while (j < n2) && (dvec2[j] == d2j) {
+                while (j < n2) && (data2[j] == d2j) {
                     d -= inv_n2;
                     j += 1;
                 }
@@ -60,34 +46,55 @@ fn rust_stats<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
         maxd - mind
     }
 
-    // wrapper of `axpy`
-    #[pyfn(m)]
-    #[pyo3(name = "axpy")]
-    fn axpy_py<'py>(
-        py: Python<'py>,
-        a: f64,
-        x: PyReadonlyArrayDyn<'py, f64>,
-        y: PyReadonlyArrayDyn<'py, f64>,
-    ) -> &'py PyArrayDyn<f64> {
-        let x = x.as_array();
-        let y = y.as_array();
-        let z = axpy(a, x, y);
-        z.into_pyarray(py)
-    }
+    fn ks_2samp_buffer(data1buffer: PyBuffer<f64>, 
+                       data2buffer: PyBuffer<f64>) -> f64 {
 
-    // wrapper of `mult`
-    #[pyfn(m)]
-    #[pyo3(name = "mult")]
-    fn mult_py<'py>(a: f64, x: &'py PyArrayDyn<f64>) {
-        let x = unsafe { x.as_array_mut() };
-        mult(a, x);
-    }
+        if (data1buffer.dimensions() != 1) ||(data2buffer.dimensions() != 1) {
+            panic!("data buffers not 1d!");
+        }
 
+        let mut mind = 0f64;
+        let mut maxd = 0f64;
+        
+        Python::with_gil(|py| {
+            let n1 = data1buffer.item_count();
+            let n2 = data2buffer.item_count();
+            let inv_n1 = 1f64 / (n1 as f64);
+            let inv_n2 = 1f64 / (n2 as f64);
 
-    #[pyfn(m)]
-    fn print_rust() -> PyResult<()> {
-        println!("test");
-        Ok(())
+            let mut d = 0f64;
+
+            let mut i = 0;
+            let mut j = 0;
+
+            let data1 = data1buffer.as_slice(py).unwrap();
+            let data2 = data2buffer.as_slice(py).unwrap();
+
+            while (i < n1) && (j < n2) {
+                let d1i = data1[i].get();
+                let d2j = data2[j].get();
+
+                if d1i <= d2j {
+                    while (i < n1) && (data1[i].get() == d1i) {
+                        d += inv_n1;
+                        i += 1;
+                    }
+                }
+
+                if d1i >= d2j {
+                    while (j < n2) && (data2[j].get() == d2j) {
+                        d -= inv_n2;
+                        j += 1;
+                    }
+                }
+                if d < mind { mind = d; }
+                if d > maxd { maxd = d; }
+                //mind = mind.min(d);
+                //maxd = maxd.max(d);
+            }
+        });
+
+        maxd - mind
     }
 
     #[pyfn(m)]
@@ -99,6 +106,15 @@ fn rust_stats<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
         let data1 = data1.as_array();
         let data2 = data2.as_array();
         ks_2samp(data1, data2)
+    }
+
+    #[pyfn(m)]
+    #[pyo3(name = "ks_2samp_buffer")]
+    fn ks_2samp_buffer_py<'py>(
+        b1: PyBuffer<f64>,
+        b2: PyBuffer<f64>,
+    ) -> f64 {
+        ks_2samp_buffer(b1, b2)
     }
 
     Ok(())
